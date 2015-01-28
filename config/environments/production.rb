c3470ee0dee5d36195f3845ec619c05ba3032b80
Emmanuel Jon A. Magdaluyo <ejmagdaluyo@gmail.com>
2015-01-28 22:21:48 +0800

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,13 +10,13 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: ENV["GMAIL_DOMAIN"],
+  address: "smtp.mail.yahoo.com",
+  port: 465,
+  domain: ENV["EMAIL_DOMAIN"],
   authentication: "plain",
   enable_starttls_auto: true,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
+  user_name: ENV["EMAIL_USERNAME"],
+  password: ENV["EMAIL_PASSWORD"]
 }
   
 
